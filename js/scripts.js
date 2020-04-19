396a37cbddfecc9fc2e0d4a9e0c974f3587a8243
postolenko <denis.postolenko@gmail.com>
2020-04-19 13:18:38 +0300

    UGL

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
+function getAnimation() {
+  $(".animate").each(function() {
+    if( $(this).offset().top <= $(document).scrollTop() + $(window).height() ) {
+      $(this).addClass("active");
+    }
+  });
+}
 
 $(window).load(function() {
 
@@ -13,45 +14,71 @@ $(window).load(function() {
 
 $(window).resize(function() {
 
-
+getAnimation();
 
 });
 
 $(document).scroll(function() {
 
-
+getAnimation();
 
 });
 
 $(document).ready(function() {
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
-            dots: true,
-            arrows: true,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            speed: 1200,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
-        });
-    }
+  getAnimation();
+
+  // ---------------------
+
+  $("[data-popup-link]").on("click", function(e) {
+      e.preventDefault();
+      popupName = $(this).attr("data-popup-link");
+      div = document.createElement('div');
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.visibility = 'hidden';
+      document.body.appendChild(div);
+      scrollWidth = div.offsetWidth - div.clientWidth;
+      document.body.removeChild(div);
+      $("body").addClass("fixed");
+      $("body").css({
+          "position" : "fixed",
+          "top" :  -$(document).scrollTop() + "px",
+          "overflow" : "hidden",
+          "right" : 0,
+          "left" : 0,
+          "bottom" : 0,
+          "padding-right" : scrollWidth + "px"
+      });      
+      $("[data-popup = '"+ popupName +"']").fadeIn(300);
+  });
+
+  $(".close_popup").on("click", function(e) {
+      e.preventDefault();
+      curTop = $("body").css("top");
+      curTop = Math.abs(parseInt(curTop, 10));
+      $("body").attr("style", "");
+      if (curTop !== 0) {
+          $("html").scrollTop(curTop);
+      }
+      $("body").removeClass("fixed");
+      $(this).closest("[data-popup]").fadeOut(300);
+  });
+
+  $(this).keydown(function(eventObject){
+      if (eventObject.which == 27 ) {
+          curTop = $("body").css("top");
+          curTop = Math.abs(parseInt(curTop, 10));
+          $("body").attr("style", "");
+          if (curTop !== 0) {
+              $("html").scrollTop(curTop);
+          }
+          $("body").removeClass("fixed");
+          $("[data-popup]").fadeOut(300);
+      }
+  });
+
+  $("input[type='tel']").mask("+7 (999) 999-99-99");
 
 });
